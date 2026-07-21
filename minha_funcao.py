0848b5bf89ec5fda96573dc924f7a4768dcef833
guilherme fachin <urbguilherme@gmail.com>
2026-07-21 17:57:47 -0300

    Add files via upload

---
+++ minha_funcao.py
@@ -0,0 +1,9 @@
+def quadrado(num):
+    return num ** 2
+
+numero = int(input("digite um numero: "))
+
+#testando a funcao
+resultado = quadrado(numero)
+
+print(f"o quadrado do numero passado por parametro é {resultado}")
